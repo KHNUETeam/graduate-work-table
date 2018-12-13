84008fe4d4338532bd0d5a39e9e9e1a22dbb6a5c
Mr. DarkWolf <jaeki94@gmail.com>
2018-12-13 02:57:16 +0200

    add requrements, connect to mysql server

--- graduate_table/settings.py
+++ graduate_table/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'graduate_table.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'graduatedb',
+        'USER': 'graduate_user',
+        'PASSWORD': 'graduate.pass0000',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
