db6952dbd302803a0f9e82d81da54fb249070d7e
Yevhenii Moskalenko <testjokerqwerty@gmail.com>
2018-12-18 00:56:30 +0200

    new des1.4

--- graduate_table/settings.py
+++ graduate_table/settings.py
@@ -82,12 +82,13 @@ DATABASES = {
     }
 }
 '''
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'graduatedb'),
-#    }
-#}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 
